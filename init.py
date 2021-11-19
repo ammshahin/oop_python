252e4934ca68fa011122aa0f33f0410a63670127
ammshahin <ammshahin@gmail.com>
2021-11-19 21:48:15 +0600

    init, assert

---
+++ init.py
@@ -0,0 +1,18 @@
+class Students:
+    def __init__(self,name: str, cgpa: float, semester: int):
+        # Run validations
+        assert cgpa >= 0, f"cgpa {cgpa} should be grater on equal to zero"
+        assert semester >= 0, f"semester {semester} should be grater on equal to zero"
+
+        #Assigned to self object
+        self.name = name
+        self.cgpa = cgpa
+        self.sem = semester
+
+
+    def show_details(self):
+        print(f"{self.name} got {self.cgpa} cgpa in {self.sem} semester")
+
+
+shahin = Students("shahin", 3.71, 10)
+shahin.show_details()
