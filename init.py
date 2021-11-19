f1a92d08cc2d868603cbc1aec0ae595c65529f88
ammshahin <ammshahin@gmail.com>
2021-11-19 21:54:47 +0600

    init, assert

--- init.py
+++ init.py
@@ -14,5 +14,5 @@ class Students:
         print(f"{self.name} got {self.cgpa} cgpa in {self.sem} semester")
 
 
-shahin = Students("meherullah shahin", 3.71, 10)
+shahin = Students("meherullah shahin", 3.71, 11)
 shahin.show_details()
